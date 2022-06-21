99062b6959cdf0b6ce28f56fdbede9fd14a89457
Zachary Carpenter <36334174+zacarpenter@users.noreply.github.com>
2022-06-21 10:46:52 -0400

    Added 2 completed Tutorials
    JokesWebApp - MVC .NET
    ZacCrafts - ASP.NET tutorial

---
+++ JokesWebApp/Models/Joke.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JokesWebApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Joke")]
+        public string JokeQuestion { get; set; }
+
+        [Display(Name = "Answer")]
+        public string JokeAnswer { get; set; }
+
+        public Joke()
+        {
+
+        }
+    }
+}
